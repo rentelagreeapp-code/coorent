e3b5e7901a19ceab6847315a0b90e863c7b7efc4
Ashok <karnatk.ashok.777@gmail.com>
2026-06-29 17:45:41 +0530

    fix: revert baseUrl to point to Render hosted production instance

--- CooRent/lib/core/constants/api_constants.dart
+++ CooRent/lib/core/constants/api_constants.dart
@@ -1,13 +1,5 @@
-import 'dart:io';
-
 class ApiConstants {
-  static String get baseUrl {
-    // 10.0.2.2 is the gateway to host localhost for Android Emulators
-    if (Platform.isAndroid) {
-      return 'http://10.0.2.2:5041';
-    }
-    return 'http://localhost:5041';
-  }
+  static const String baseUrl = 'https://coorent.onrender.com';
   
   static const String sendOtp = '/api/auth/send-otp';
   static const String verifyOtp = '/api/auth/verify-otp';
