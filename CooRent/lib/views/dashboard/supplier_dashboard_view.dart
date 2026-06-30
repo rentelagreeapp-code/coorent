c1e10e886daef496ccd7848ec0a70b6c04ae5279
Ashok <karnatk.ashok.777@gmail.com>
2026-06-30 09:53:37 +0530

    fix: import dio in supplier_dashboard_view to resolve compiling exceptions

--- CooRent/lib/views/dashboard/supplier_dashboard_view.dart
+++ CooRent/lib/views/dashboard/supplier_dashboard_view.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:typed_data';
+import 'package:dio/dio.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:image/image.dart' as img;
 import 'package:flutter/material.dart';
